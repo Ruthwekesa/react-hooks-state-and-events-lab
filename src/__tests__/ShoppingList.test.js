7d67dbb9d82230e3c0ea00aa98f38c187380d3ba
ruthwekesa <wekesaruth569@gmail.com>
2022-09-24 17:56:41 +0300

    Assignment completed

--- src/__tests__/ShoppingList.test.js
+++ src/__tests__/ShoppingList.test.js
@@ -3,32 +3,34 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import ShoppingList from "../components/ShoppingList";
 
 const testData = [
-  { id: 1, name: "Yogurt", category: "Dairy" },
-  { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Cookies", category: "Dessert" },
+    { id: 1, name: "Yogurt", category: "Dairy" },
+    { id: 2, name: "Pomegranate", category: "Produce" },
+    { id: 3, name: "Lettuce", category: "Produce" },
+    { id: 4, name: "String Cheese", category: "Dairy" },
+    { id: 5, name: "Cookies", category: "Dessert" },
 ];
 
 test("displays all items when initially rendered", () => {
-  const { container } = render(<ShoppingList items={testData} />);
-  expect(container.querySelector(".Items").children).toHaveLength(
-    testData.length
-  );
-});
+            const { container } = render( < ShoppingList items = { testData }
+                />);
+                expect(container.querySelector(".Items").children).toHaveLength(
+                    testData.length
+                );
+            });
 
-test("displays only items that match the selected category", () => {
-  const { container } = render(<ShoppingList items={testData} />);
+        test("displays only items that match the selected category", () => {
+                const { container } = render( < ShoppingList items = { testData }
+                    />);
 
-  fireEvent.change(screen.getByRole("combobox"), {
-    target: { value: "Dairy" },
-  });
+                    fireEvent.change(screen.getByRole("combobox"), {
+                        target: { value: "Dairy" },
+                    });
 
-  expect(container.querySelector(".Items").children).toHaveLength(2);
+                    expect(container.querySelector(".Items").children).toHaveLength(2);
 
-  fireEvent.change(screen.getByRole("combobox"), {
-    target: { value: "Dessert" },
-  });
+                    fireEvent.change(screen.getByRole("combobox"), {
+                        target: { value: "Dessert" },
+                    });
 
-  expect(container.querySelector(".Items").children).toHaveLength(1);
-});
+                    expect(container.querySelector(".Items").children).toHaveLength(1);
+                });
